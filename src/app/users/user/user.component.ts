72a1a0214c796a920dec2a5af0275b14b80379d8
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-06-17 09:29:29 +0530

    Commit for Fetching Route Parameters

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-user',
@@ -9,9 +10,13 @@ export class UserComponent implements OnInit {
 
   user: {id: number, name: string};
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    };
   }
 
 }
